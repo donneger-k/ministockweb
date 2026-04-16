c41a8585fa4e36724ef12fbd384ae0e034ea9fcc
DONNEGER Kilyan <kilyan.donneger@gmail.com>
2026-04-16 11:33:43 +0200

    chore: project cleanup

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    return Inertia::render('welcome');
 });
-
-require __DIR__.'/settings.php';
