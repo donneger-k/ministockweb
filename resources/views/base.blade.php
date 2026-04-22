5b4b91405d9f46e1b5233878ceb580b1f37cfe9d
DONNEGER Kilyan <kilyan.donneger@gmail.com>
2026-04-22 13:54:31 +0200

    feat: add global navbar

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,9 +8,28 @@
 </head>
 <body>
 
-    <h1 class="text-center" >Ministock</h1>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href='{{ route('home') }}'>Ministock</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    @yield('content')
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('stock') }}">Stock</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main class="container">
+        <h1 class="text-center" > @yield('titre')</h1>
+
+        @yield('content')
+    </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
