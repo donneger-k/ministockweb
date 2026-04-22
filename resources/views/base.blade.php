15fb21ccc44c2d5a1e40985f249574c32b51ae27
DONNEGER Kilyan <kilyan.donneger@gmail.com>
2026-04-22 11:36:09 +0200

    feat: integrate Bootstrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ministock</title>
 </head>
 <body>
 
-    <h1>Ministock</h1>
+    <h1 class="text-center" >Ministock</h1>
 
     @yield('content')
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
